429053c142cefa7893052b60143783342da3dac1
Guilherme Daniel <guilherme.daniel@inova.business>
2024-01-18 20:07:29 +0000

    project structure

---
+++ src/main/java/com/gd/edugrady/EduGradyApplication.java
@@ -0,0 +1,13 @@
+package com.gd.edugrady;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EduGradyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EduGradyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gd/edugrady/EduGradyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gd.edugrady;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EduGradyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
